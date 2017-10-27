de11976aac6f79be5e8eebac41b72052e2abe1a2
facundocastello <facundoacastello@gmail.com>
2017-10-27 16:19:45 -0300

    agregue comparacion de varios matches por imagen

--- UAVAgroStateStitcher.h
+++ UAVAgroStateStitcher.h
@@ -18,6 +18,7 @@ bool sortByDist(DMatch a, DMatch b){
 }
 
 
+
 class UAVAgroStateStitcher{
 	public:
 		int tamano;
@@ -128,24 +129,51 @@ class UAVAgroStateStitcher{
 			return Kp;
 		}
 
-
-		vector<Mat> matchAndTransform(Mat descriptors_1, Mat descriptors_2,
-			vector<KeyPoint> keypoints_1, vector<KeyPoint> keypoints_2 )
-		{
-			Mat H;
+		vector< DMatch > matchKp(Mat descriptors_1, Mat descriptors_2){
 			BFMatcher matcher(NORM_HAMMING,true);
 			vector< DMatch > matches;
 			matcher.match(descriptors_1, descriptors_2, matches);
-			sort(matches.begin(), matches.end(), sortByDist);
 			cout << "Cantidad de matches: "<<matches.size()<<" \n";
-			int matchSize = (matches.size() >=30)?30 : matches.size();
-			vector< DMatch > good_matches(matches.begin(),matches.begin() + matchSize);
+			return matches;
+		}
+
+		vector< DMatch > compareMatch(){
+
+		}
+
+
+		vector<Mat> matchAndTransform(vector<Mat> descriptors,vector< vector<KeyPoint> > keypoints)
+		{
+			Mat H;
+			vector< vector< DMatch > > matches;
+			for(int i = 1;i < descriptors.size() ; i++){
+				matches.push_back(this->matchKp(descriptors[0],descriptors[i]));
+			}
+			vector< DMatch > good_matches;
+			int maxMatch = 60;
+			int matchSize = (matches[0].size() >=(maxMatch/matches.size()) )?(maxMatch/matches.size()) : matches[0].size();
+			for(int i = 1;i < matches.size() ; i++){
+				vector< DMatch > matchAux;
+				for(int j = 1; j < matches[0].size();j++){
+					for(int k = 1;k < matches[i].size();k++){
+						if(matches[0][j].queryIdx ==  matches[i][k].queryIdx){
+							matchAux.push_back(matches[0][j]);
+						}
+					}
+				}
+				sort(matchAux.begin(), matchAux.end(), sortByDist);
+				good_matches.insert(good_matches.end(),matchAux.begin(),matchAux.begin() + matchSize);
+				cout<< "matchaux size: " << matchAux.size() << "goodmatches" << good_matches.size() << endl;
+			}
+			
+			sort(matches[0].begin(), matches[0].end(), sortByDist);
+			good_matches.insert(good_matches.end(),matches[0].begin(),matches[0].begin() + matchSize);
 			///OBTENGO LOS PUNTOS EN LOS QUE SE ENCUENTRAN LOS GOOD MATCHES
 			std::vector<Point2f> obj;
 			std::vector<Point2f> scene;
 			for (int i = 0; i < good_matches.size(); i++) {
-				obj.push_back(keypoints_1[good_matches[i].queryIdx].pt);
-				scene.push_back(keypoints_2[good_matches[i].trainIdx].pt);
+				obj.push_back(keypoints[0][good_matches[i].queryIdx].pt);
+				scene.push_back(keypoints[1][good_matches[i].trainIdx].pt);
 			}
 			// ARMO LA MATRIZ DE HOMOGRAFIA EN BASE A LOS PUNTOS ANTERIORES
 			Mat maskH;
@@ -179,9 +207,22 @@ class UAVAgroStateStitcher{
 			FileStorage fsHomo("Data/Homografias/homografias.yml", FileStorage::WRITE);
 			for (int i = 0; i < strImgs.size()-1; i++){
 				cout << "Match y Homografia de "+strImgs[i]+" y " + strImgs[i+1] + " :" <<endl;
-				vector<Mat> aux = matchAndTransform(
-					vecDesc[i], vecDesc[i+1],
-					vecKp[i], vecKp[i+1]);
+				vector<Mat> aux;
+				if((vecDesc.size()-i-2) > 0 ) {
+					if((vecDesc.size()-i-3) > 0 ) {
+						aux = matchAndTransform(
+							{vecDesc[i], vecDesc[i+1],vecDesc[i+2],vecDesc[i+3]},
+							{vecKp[i], vecKp[i+1],vecKp[i+2],vecKp[i+3]});	
+					}else{
+						aux = matchAndTransform(
+							{vecDesc[i], vecDesc[i+1],vecDesc[i+2]},
+							{vecKp[i], vecKp[i+1],vecKp[i+2]});	
+					}
+				}else{
+					aux = matchAndTransform(
+						{vecDesc[i], vecDesc[i+1]},
+						{vecKp[i], vecKp[i+1]});
+				}
 				//Una homografia es para calcular el boundbox (H) y la otra es para
 				//con ese boundbox calcular las otras homografias, multiplicandolas 
 				//esta es homonomultpilicates
@@ -222,7 +263,7 @@ class UAVAgroStateStitcher{
 
 			Mat img1 = CommonFunctions::cargarImagen(strImgs[1] , this->tamano);
 
-			vector<Mat> aux = matchAndTransform(descriptors,vecDesc[1],keypoints,vecKp[1]);
+			vector<Mat> aux = matchAndTransform({descriptors,vecDesc[1]},{keypoints,vecKp[1]});
 			H[1] = aux[0];
 			for(int i = 2 ; i < H.size(); i++){
 				H[i]=H[i-1] * homoNoMultiplicated[i];
