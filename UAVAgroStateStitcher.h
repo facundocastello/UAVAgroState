ed8509ca0772fc06c78622c180f80a5ed940083b
facundocastello <facundoacastello@gmail.com>
2017-10-28 14:36:32 -0300

    agregue paralelismo

--- UAVAgroStateStitcher.h
+++ UAVAgroStateStitcher.h
@@ -84,27 +84,31 @@ class UAVAgroStateStitcher{
 			///CALCULA LOS KEYPOINTS Y DESCRIPTORES DE CADA IMAGEN
 			//-- Step 1 and 2 : Detect the keypoints and Calculate descriptors 
 			Ptr<cv::BRISK> orb = cv::BRISK::create(this->kPoints);
-			FileStorage fsDesc("Data/DetectCompute/descriptores.yml", FileStorage::WRITE);
-			
-			for(int i = 0;i < imgs.size() ; i++){
-				cout << "--------------------------------------------------------" << endl;
-				cout << "Calculando KeyPoints y descriptores de la imagen" + to_string(i) +": ... ";
-				std::vector<KeyPoint> keypoints;
-				Mat descriptors;
-				orb->detectAndCompute(imgs[i] , Mat() , keypoints , descriptors);
-				fsDesc << "descriptor"+ to_string(i) << descriptors;
-				fsDesc << "features" + to_string(i)  << "[";
-				for( int i = 0; i < keypoints.size(); i++ )
-				{
-					fsDesc << "{:" << "x" << keypoints[i].pt.x << "y" << keypoints[i].pt.y << "}";
+			// FileStorage fsDesc("Data/DetectCompute/descriptores.yml", FileStorage::WRITE);
+			vecDesc = vector<Mat>(imgs.size());
+			vecKp = vector< vector<KeyPoint> >(imgs.size());
+			cout << vecKp.size() <<endl;
+
+			parallel_for_(Range(0, imgs.size()), [&](const Range& range){
+				for(int i = range.start;i < range.end ; i++){
+					std::vector<KeyPoint> keypoints;
+					Mat descriptors;
+					orb->detectAndCompute(imgs[i] , Mat() , keypoints , descriptors);
+					// fsDesc << "descriptor"+ to_string(i) << descriptors;
+					// fsDesc << "features" + to_string(i)  << "[";
+					// for( int i = 0; i < keypoints.size(); i++ )
+					// {
+					// 	fsDesc << "{:" << "x" << keypoints[i].pt.x << "y" << keypoints[i].pt.y << "}";
+					// }
+					// fsDesc << "]";
+					vecDesc[i]=descriptors;
+					vecKp[i]=keypoints;
+					cout << "\n KeyPoint imagen" + to_string(i) + ": " << keypoints.size()<< endl;
+					// begin = CommonFunctions::tiempo(begin, "Tiempo para kp y descriptores" + to_string(i) + ": ");
 				}
-				fsDesc << "]";
-				vecDesc.push_back(descriptors);
-				vecKp.push_back(keypoints);
-				cout << "\n KeyPoint imagen" + to_string(i) + ": " << keypoints.size()<< endl;
-				// begin = CommonFunctions::tiempo(begin, "Tiempo para kp y descriptores" + to_string(i) + ": ");
-			}
-			fsDesc.release();
+			});
+
+			// fsDesc.release();
 		}
 		Mat readDetectAndComputeDesc(int i){
 			clock_t begin = clock();
@@ -134,7 +138,7 @@ class UAVAgroStateStitcher{
 			BFMatcher matcher(NORM_HAMMING,true);
 			vector< DMatch > matches;
 			matcher.match(descriptors_1, descriptors_2, matches);
-			cout << "Cantidad de matches: "<<matches.size()<<" \n";
+			// cout << "Cantidad de matches: "<<matches.size()<<" \n";
 			return matches;
 		}
 
@@ -150,6 +154,7 @@ class UAVAgroStateStitcher{
 			for(int i = 1;i < descriptors.size() ; i++){
 				matches.push_back(this->matchKp(descriptors[0],descriptors[i]));
 			}
+						
 			vector< DMatch > matchAux;
 			int maxMatch = 30;
 			for(int i = 1;i < matches.size() ; i++){
@@ -169,7 +174,7 @@ class UAVAgroStateStitcher{
 						}
 					}
 				}
-				cout<< "matchaux size: " << matchAux.size();
+				// cout<< "matchaux size: " << matchAux.size() << endl;
 			}
 			sort(matchAux.begin(), matchAux.end(), sortByDist);
 			sort(matches[0].begin(),matches[0].end(),sortByDist);
@@ -186,7 +191,7 @@ class UAVAgroStateStitcher{
 					good_matches.push_back(matches[0][i]);
 				}
 			}
-			cout<< "goodmatches" << good_matches.size() << endl;
+			// cout<< "goodmatches" << good_matches.size() << endl;
 			///OBTENGO LOS PUNTOS EN LOS QUE SE ENCUENTRAN LOS GOOD MATCHES
 			std::vector<Point2f> obj;
 			std::vector<Point2f> scene;
@@ -198,21 +203,21 @@ class UAVAgroStateStitcher{
 			Mat maskH;
 			H = findHomography(scene, obj, CV_RANSAC);
 			/// DEVUELVO H
-			cout << "--------------------------------------------------------" << endl;
+			// cout << "--------------------------------------------------------" << endl;
 			return{ H };
 		}
 
 		Mat stitchImgs(vector<string> strImgs){
 			string normal = "\033[0m";
 			string process = "\033[1;32m";
-			vector<Mat> H;
-			vector<Mat> homoNoMultiplicated;
+			vector<Mat> H(strImgs.size());
+			vector<Mat> homoNoMultiplicated(strImgs.size());
 			clock_t begin = clock();
 			//AGREGO INDENTACION
 
 			//CALCULO HOMOGRAFIAS PARA CADA IMAGEN
-			H.push_back(Mat::eye(3, 3, CV_64F));
-			homoNoMultiplicated.push_back(Mat::eye(3, 3, CV_64F));
+			H[0] = (Mat::eye(3, 3, CV_64F));
+			homoNoMultiplicated[0] = (Mat::eye(3, 3, CV_64F));
 			double xMin=0;double xMax=0;double yMin=0;double yMax=0;
 			//obtengo kp
 			vector<Mat> imgs = CommonFunctions::cargarImagenes(strImgs , this->tamano);
@@ -223,38 +228,45 @@ class UAVAgroStateStitcher{
 			saveDetectAndCompute(imgs, vecDesc, vecKp);
 			cout << process + "-|-|-|-|-|-|-|-|-|-|-|-|-|Matcheando y obteniendo Homografias: " + normal << endl;
 			//usado para documentar las homografias
-			FileStorage fsHomo("Data/Homografias/homografias.yml", FileStorage::WRITE);
-			for (int i = 0; i < strImgs.size()-1; i++){
-				cout << "Match y Homografia de "+strImgs[i]+" y " + strImgs[i+1] + " :" <<endl;
-				vector<Mat> aux;
-				int nMin;
-				(i<3)?nMin=i:nMin=3;
-				int nMax;
-				int nMaxVal=3;
-				((vecDesc.size()-i-1) < nMaxVal )? nMax=(vecDesc.size()-i-1) : nMax=nMaxVal;
+			parallel_for_(Range(0, strImgs.size()-1), [&](const Range& range){
+				for(int i = range.start;i < range.end ; i++){
+					cout << "Empezo Match y Homografia de "+strImgs[i]+" y " + strImgs[i+1] +":" <<endl;
+					// cout << "Empezo Match y Homografia de "+strImgs[i]+" y " + strImgs[i+1] + " :" <<endl;
+					vector<Mat> aux;
+					int nMin;
+					(i<3)?nMin=i:nMin=3;
+					int nMax;
+					int nMaxVal=3;
+					((vecDesc.size()-i-1) < nMaxVal )? nMax=(vecDesc.size()-i-1) : nMax=nMaxVal;
 
-				vector<Mat> newVecDesc;
-				vector< vector<KeyPoint> > newVecKp;
-				newVecDesc.push_back(vecDesc[i]);
-				newVecKp.push_back(vecKp[i]);
-				// for(int j=1;j <= nMin;j++){
-				// 	newVecDesc.push_back(vecDesc[i-j]);
-				// 	newVecKp.push_back(vecKp[i-j]);
-				// }
-				for(int j=1;j <= nMax;j++){
-					newVecDesc.push_back(vecDesc[i+j]);
-					newVecKp.push_back(vecKp[i+j]);
+					vector<Mat> newVecDesc;
+					vector< vector<KeyPoint> > newVecKp;
+					newVecDesc.push_back(vecDesc[i]);
+					newVecKp.push_back(vecKp[i]);
+					// for(int j=1;j <= nMin;j++){
+					// 	newVecDesc.push_back(vecDesc[i-j]);
+					// 	newVecKp.push_back(vecKp[i-j]);
+					// }
+					for(int j=1;j <= nMax;j++){
+						newVecDesc.push_back(vecDesc[i+j]);
+						newVecKp.push_back(vecKp[i+j]);
+					}
+					aux = matchAndTransform(newVecDesc,newVecKp);
+					
+					//Una homografia es para calcular el boundbox (H) y la otra es para
+					//con ese boundbox calcular las otras homografias, multiplicandolas 
+					//esta es homonomultpilicates
+					homoNoMultiplicated[i+1] = (aux[0]);
+					//Encuentro el maximo y minimo tanto en x como en y de todas las 
+					//homografias para despues poder hacer el bounding box;
+					cout << "Termino Match y Homografia de "+strImgs[i]+" y " + strImgs[i+1] + " :" <<endl;
+					
 				}
-				aux = matchAndTransform(newVecDesc,newVecKp);
-
-				//Una homografia es para calcular el boundbox (H) y la otra es para
-				//con ese boundbox calcular las otras homografias, multiplicandolas 
-				//esta es homonomultpilicates
-				H.push_back(H[i] * aux[0]);
+			});
+			FileStorage fsHomo("Data/Homografias/homografias.yml", FileStorage::WRITE);
+			for(int i = 0; i < strImgs.size()-1;i++){
+				H[i+1] = (H[i] * homoNoMultiplicated[i+1]);
 				H[i+1] = H[i+1] / H[i+1].at<double>(2,2);
-				homoNoMultiplicated.push_back(aux[0]);
-				//Encuentro el maximo y minimo tanto en x como en y de todas las 
-				//homografias para despues poder hacer el bounding box;
 				if(H[i+1].at<double>(0,2) < xMin){
 					xMin = H[i+1].at<double>(0,2);
 					// cout << "xmin " << xMin << endl;
@@ -273,7 +285,7 @@ class UAVAgroStateStitcher{
 				}
 				fsHomo << "homografia"+to_string(i+1) << H[i+1];
 			}
-			fsHomo.release();
+			// fsHomo.release();
 
 			Mat boundBox = CommonFunctions::cargarImagen(strImgs[0], this->tamano);;
 			boundBox = CommonFunctions::boundingBox(boundBox, (yMin * -1) + boundBox.cols, yMax , (xMin * -1),xMax);
